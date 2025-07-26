e5345ed5d2f5757a3bbd9535c890bae3b186500e
akashgit4545 <akkipatil002325@gmail.com>
2025-07-26 13:38:17 +0530

    added golu

--- cypress/integration/web/basic.spec.ts
+++ cypress/integration/web/basic.spec.ts
@@ -12,5 +12,8 @@ describe('start with basics', () => {
         cy.visit('/');
         basicPage.validateGoogleLogo();
         basicPage.validateGoogleSearchTab();
+        cy.visit('/');
+        basicPage.validateGoogleLogo();
+        basicPage.validateGoogleSearchTab();
     })
 })
